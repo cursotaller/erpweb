7233492d241a389a12eb87c4af6f347e449b99f8
asullom <asullom@gmail.com>
2015-11-01 13:14:22 -0500

    categoria admin

--- ew_almacen/admin.py
+++ ew_almacen/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from ew_almacen.models import Insumo
+from ew_almacen.models import Insumo, Categoria
 
 # Register your models here.
+
+admin.site.register(Categoria)
 
 
 class InsumoAdmin(admin.ModelAdmin):
     search_fields = ('codigo', 'descripcion',)
-    list_display = ('codigo', 'descripcion', 'precio')
+    list_display = ('codigo', 'descripcion', 'precio', 'categoria')
 
 admin.site.register(Insumo, InsumoAdmin)
